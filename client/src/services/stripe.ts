11b302ae51e7915498f66b548d43870fc744a3f0
Lucas Fernando de Souza <lucasnando800@hotmail.com>
2021-12-22 22:35:56 -0300

    configuracao stripe

---
+++ client/src/services/stripe.ts
@@ -0,0 +1,15 @@
+import Stripe from 'stripe';
+import packageInfos from '../../package.json'
+
+const {version} = packageInfos;
+
+export const stripe = new Stripe(
+  process.env.STRIPE_API_KEY,
+  {
+    apiVersion: '2020-08-27',
+    appInfo: {
+      name: "Technews",
+      version
+    }
+  }
+);
